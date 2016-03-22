ccc236d44c977197a2b31e7362243bd744f81905
James Turello <jturello@yahoo.com>
2016-03-22 06:44:39 -0700

    add integration tests for Venues page

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -70,92 +70,45 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).doesNotContain("secondBand");
   }
 
+  @Test
+  public void clickBandLink_navigatesToBandPage_true() {
+    goTo("http://localhost:4567/");
+    fill("#name").with("firstBand");
+    fill("#genre").with("firstGenre");
+    submit("#addBandBtn");
+    click("a", withText("firstBand"));
+    assertThat(pageSource()).contains("Concerts/venues the band has played");
+  }
 
-  // @Test
-  // public void addTagToRecipe() {
-  //   Tag newTag = new Tag("Mexican");
-  //   newTag.save();
-  //   Recipe newRecipe = new Recipe("Tacos");
-  //   newRecipe.save();
-  //   String recipePath = String.format("http://localhost:4567/recipes/%d", newRecipe.getId());
-  //   goTo(recipePath);
-  //   assertThat(pageSource()).contains("Mexican");
-  //   assertThat(pageSource()).contains("Tacos");
-  // }
-  //
-  // @Test
-  // public void addRecipeToTag() {
-  //   Tag newTag = new Tag("Mexican");
-  //   newTag.save();
-  //   Recipe newRecipe = new Recipe("Tacos");
-  //   newRecipe.save();
-  //   String tagPath = String.format("http://localhost:4567/tags/%d", newTag.getId());
-  //   goTo(tagPath);
-  //   assertThat(pageSource()).contains("Mexican");
-  //   assertThat(pageSource()).contains("Tacos");
-  // }
+  @Test
+  public void addVenue_fromBandPageRedisplaysBandPage_true() {
+    goTo("http://localhost:4567/");
+    fill("#name").with("firstBand");
+    fill("#genre").with("firstGenre");
+    submit("#addBandBtn");
+    click("a", withText("firstBand"));
+    fill("#name").with("Red Rocks Amphitheater");
+    fill("#location").with("Denver");
+    submit("#addVenueBtn");
+    assertThat(pageSource()).contains("Concerts/venues the band has played");
+  }
 
-  //
-  // @Test
-  // public void deleteTag() {
-  //   Tag newTag = new Tag("Mexican");
-  //   newTag.save();
-  //   String tagId = "#" + newTag.getId();
-  //   goTo("http://localhost:4567/tags");
-  //   submit(tagId);
-  //   assertThat(pageSource()).doesNotContain(newTag.getTitle());
-  //   assertThat(pageSource()).doesNotContain("404");
-  // }
-  //
-  // @Test
-  // public void assignTagToRecipe() {
-  //   Tag newTag = new Tag("Mexican");
-  //   newTag.save();
-  //   Recipe newRecipe = new Recipe("Tacos");
-  //   newRecipe.save();
-  //   // newRecipe.addTag(newTag);
-  //   goTo("http://localhost:4567/recipes/" + newRecipe.getId());
-  //   fillSelect("#tagTitle").withIndex(0);
-  //   submit("#assignTagBtn");
-  //   assertThat(pageSource()).contains("<a href=\"/tags/" + newTag.getId() + "\">");
-  //
-  // }
-  //
-  // @Test
-  // public void assignRecipeToTag() {
-  //   Tag newTag = new Tag("Mexican");
-  //   newTag.save();
-  //   Recipe newRecipe = new Recipe("Tacos");
-  //   newRecipe.save();
-  //   // newRecipe.addTag(newTag);
-  //   goTo("http://localhost:4567/tags/" + newTag.getId());
-  //   fillSelect("#recipe_id").withIndex(0);
-  //   submit("#assignRecipeBtn");
-  //   assertThat(pageSource()).contains("<a href=\"/recipes/" + newRecipe.getId() + "\">");
-  //
-  // }
-  //
-  // @Test
-  // public void deleteAllRecipes_displaysNoRecipesOnRecipePage() {
-  //   Recipe newRecipe = new Recipe("Tacos");
-  //   newRecipe.save();
-  //   goTo("http://localhost:4567/recipes");
-  //   submit("#deleteAllRecipesBtn");
-  //   assertThat(pageSource()).doesNotContain("Tacos");
-  //   assertThat(pageSource()).doesNotContain("Error");
-  // }
-  //
-  // @Test
-  // public void updateAllRecipes_updatesNameAndIngredients() {
-  //   Recipe newRecipe = new Recipe("Tacos", "Beef");
-  //   newRecipe.save();
-  //
-  //   goTo("http://localhost:4567/recipes/" + newRecipe.getId());
-  //   fill("#updateRecipeTitle").with("UpdatedTitle");
-  //   fill("#updateRecipeIngredients").with("Yak Butt");
-  //   submit("#updateRecipe");
-  //   assertThat(pageSource()).contains("UpdatedTitle");
-  //   assertThat(pageSource()).contains("Yak Butt");
-  // }
-  //
+    @Test
+    public void addVenue_fromVenuesPageRedisplaysVenuesPage_true() {
+      goTo("http://localhost:4567/venues");
+      fill("#name").with("Red Rocks Amphitheater");
+      fill("#location").with("Denver");
+      submit("#addVenueBtn");
+      assertThat(pageSource()).contains("Venue List");
+    }
+
+    @Test
+    public void addVenue_addedVenueDisplaysInVenuelist_true() {
+      goTo("http://localhost:4567/venues");
+      fill("#name").with("newVenue");
+      fill("#location").with("someLocation");
+      submit("#addVenueBtn");
+      assertEquals(find("#venueList").find("li").getText(), "newVenue");
+    }
+
 } // END OF AppTest CLASS
